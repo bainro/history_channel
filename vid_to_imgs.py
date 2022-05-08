db5b4602ea2389349194a1d3ca096a9dd87b744e
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-07 19:13:24 -0700

    Update vid_to_imgs.py

--- vid_to_imgs.py
+++ vid_to_imgs.py
@@ -1,7 +1,10 @@
 # script to convert video into individual frames
-import subprocess
+import subprocess as sp
 import sys
 import os
+
+cmd='ffmpeg -i inputVideo outputFrames_%04d.sgi'
+sp.call(cmd,shell=True)
 
 def main(args):
     assert len(args) == 2, "specify output directory & input video"
@@ -12,15 +15,10 @@ def main(args):
     # maintains aspect ratio
     res = "1920:-1"
     
-    cwd = os.getcwd()
     bash_cmd = f"ffmpeg -i \"{input_v}\" -vf scale=\"{res}\""
-    bash_cmd += f" -o \"{output_dir}/frame_%05d.jpg\""
+    bash_cmd += f" \"{output_dir}/frame_%05d.jpg\""
     print("bash command: ", bash_cmd)
-    bash_cmd = bash_cmd.split()
-    process = subprocess.Popen(bash_cmd, stdout=subprocess.PIPE, cwd=cwd)
-    output, error = process.communicate()
-    print("output: ", output)
-    print("error: ", error)
+    sp.call(bash_cmd, shell=True)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
