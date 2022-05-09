b89d5805cf23bbcbd7b3ab1ea7ab49daa4a346df
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-08 22:02:58 -0700

    Update gfr_conversion.py

--- gfr_conversion.py
+++ gfr_conversion.py
@@ -33,6 +33,7 @@ for subset in tqdm(train_subsets, disable=DIS_TQDM):
         if file_ext == ".txt": continue
         frame_id = img_name.split("/")[-1].lstrip("0")  
         # offset since filenames start at 1, not 0
+        print(frame_id)
         frame_id = int(frame_id) - 1
         new_frame = cv2.imread(img_file)
 
