8e48f4e2c704c6d1cea24f4f5df0b6ba1fba9276
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-09 12:09:43 -0700

    Update gfr_conversion.py

--- gfr_conversion.py
+++ gfr_conversion.py
@@ -48,8 +48,11 @@ for subset in tqdm(train_subsets, disable=DIS_TQDM):
         if file_ext == ".txt": continue
         frame_id = img_name.split("/")[-1]
         frame_id = frame_id[6:].lstrip("0")
-        # offset since filenames start at 1, not 0
-        frame_id = int(frame_id) - 1
+        try:
+            # offset since filenames start at 1, not 0
+            frame_id = int(frame_id) - 1
+        except:
+            print(img_name)
         # the frame rate was 5 FPS
         frame_id = int(frame_id / 12)
         new_frame = cv2.imread(img_file)
